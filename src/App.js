f8f0e0c4709e0155e074631a46775fd6069eeb29
tishreddy <u19072211@tuks.co.za>
2026-01-15 15:41:27 +0200

    All routing correct

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
-import Menu from './pages/Menu';
+import Products from './pages/Products';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import './App.css';
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,12 +3,13 @@ import './Footer.css';
 function Footer() {
   return (
     <footer className="footer">
-      <div className="footer-content"> //find out details from clients
-        <p>Contact: [phone number]</p>
-        <p>Email: [bakery email]</p>
-        <p>Instagram: @blossomsbakery</p>
-        <p>&copy; 2026 Blossoms Bakery. All rights reserved.</p>
-      </div>
+        <div className="footer-content"> 
+            {/* find out details from clients */}
+            <p>Contact: [phone number]</p>
+            <p>Email: [bakery email]</p>
+            <p>Instagram: @blossomsbakery</p>
+            <p>&copy; 2026 Blossoms Bakery. All rights reserved.</p>
+        </div>
     </footer>
   );
 }
